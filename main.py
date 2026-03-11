7b9b838b04659588a60ca73a2d5c72dbc4c19bb3
Sandeep Maurya <sandeep.maurya@aeris.net>
2026-03-11 20:11:09 +0530

    Add ChatOllama integration and summary generation to main application

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import os
+from http.client import responses
 
 from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import ChatOllama
 
 
 def main():
-    print("Hello from langchain-course!")
-    print("Loading environment variables from .env file...")
     load_dotenv()
-    api_key = os.getenv("OPENAI_API_KEY")
-    if api_key:
-        print("API_KEY loaded successfully!")
-    else:
-        print("API_KEY not found. Please check your .env file.")
+
+    # llm = ChatGoogleGenerativeAI(
+    #     model="gemini-2.5-flash",
+    #     temperature=0.3
+    # )
+
+    llm = ChatOllama(
+        model="gemma3:270m",
+        temperature=0.3
+    )
+
+    article_content = """
+    Generative AI is transforming the landscape of software engineering by automating repetitive coding tasks. 
+    Tools like GitHub Copilot and Gemini are now integrated directly into IDEs, allowing developers to generate 
+    boilerplate code, debug complex algorithms, and even write unit tests. While some fear the displacement 
+    of human roles, most experts agree that AI acts as a 'force multiplier,' enabling engineers to focus 
+    on high-level system architecture and problem-solving rather than syntax.
+    """
+    prompt = ChatPromptTemplate.from_messages([
+        ("system",
+         "You are an expert editorial assistant. Your task is to provide a concise, professional summary of the provided text."),
+        ("human",
+         "Please summarize the following article into {word_count} words or less, focusing on the core thesis and key takeaways:\n\n{article_text}")
+    ])
+    chain = prompt | llm | StrOutputParser()
+    response = chain.invoke(input={
+        "article_text": article_content,
+        "word_count": "50"
+    })
+    print("Summary:", response)
 
 
 if __name__ == "__main__":
