dffb940475ae36fabf713da597e0f1c3081dcc92
Sandeep Maurya <sandeep.maurya@aeris.net>
2026-03-12 10:27:16 +0530

    Add Tavily search integration and refactor main application logic

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
-import os
-from http.client import responses
-
 from dotenv import load_dotenv
-from langchain_core.output_parsers import StrOutputParser
+load_dotenv()
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_ollama import ChatOllama
+from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+
+tavilyClient = TavilyClient()
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: search query
+    Returns:
+        search results
+    """
+    print(f"Searching for: {query}")
+    return tavilyClient.search(query=query)
 
 
 def main():
-    load_dotenv()
 
-    # llm = ChatGoogleGenerativeAI(
-    #     model="gemini-2.5-flash",
-    #     temperature=0.3
-    # )
-
-    llm = ChatOllama(
-        model="gemma3:270m",
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
         temperature=0.3
     )
-
-    article_content = """
-    Generative AI is transforming the landscape of software engineering by automating repetitive coding tasks. 
-    Tools like GitHub Copilot and Gemini are now integrated directly into IDEs, allowing developers to generate 
-    boilerplate code, debug complex algorithms, and even write unit tests. While some fear the displacement 
-    of human roles, most experts agree that AI acts as a 'force multiplier,' enabling engineers to focus 
-    on high-level system architecture and problem-solving rather than syntax.
-    """
-    prompt = ChatPromptTemplate.from_messages([
-        ("system",
-         "You are an expert editorial assistant. Your task is to provide a concise, professional summary of the provided text."),
-        ("human",
-         "Please summarize the following article into {word_count} words or less, focusing on the core thesis and key takeaways:\n\n{article_text}")
-    ])
-    chain = prompt | llm | StrOutputParser()
-    response = chain.invoke(input={
-        "article_text": article_content,
-        "word_count": "50"
-    })
-    print("Summary:", response)
+    # llm = ChatOllama(
+    #     model="gemma3:270m",
+    #     temperature=0.3
+    # )
+    agent = create_agent(llm, tools=[TavilySearch()])
+    response = agent.invoke({"messages": HumanMessage("What is the weather in Tokio?")})
+    print(response)
 
 
 if __name__ == "__main__":
