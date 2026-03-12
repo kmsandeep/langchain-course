e7e8584f351d5144a4f909f3182995895da2393c
Sandeep Maurya <sandeep.maurya@aeris.net>
2026-03-12 10:43:33 +0530

    Refactor agent implementation to use Pydantic models for structured output and remove deprecated search function

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
+from typing import List
+
 from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+
 load_dotenv()
 from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
-from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
-from tavily import TavilyClient
 from langchain_tavily import TavilySearch
 
-tavilyClient = TavilyClient()
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: search query
-    Returns:
-        search results
-    """
-    print(f"Searching for: {query}")
-    return tavilyClient.search(query=query)
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="Thr agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answer"
+    )
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
-
-    llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
-        temperature=0.3
+    print("Hello from langchain-course!")
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="What are the latest news about the US economy?"
+            )
+        }
     )
-    # llm = ChatOllama(
-    #     model="gemma3:270m",
-    #     temperature=0.3
-    # )
-    agent = create_agent(llm, tools=[TavilySearch()])
-    response = agent.invoke({"messages": HumanMessage("What is the weather in Tokio?")})
-    print(response)
+    print(result)
 
 
 if __name__ == "__main__":
